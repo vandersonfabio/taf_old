644bdcd75a79563f1c8d66c22ea35d39a849a81d
vandersonfabio <vanderson.fabio@gmail.com>
2021-11-11 15:21:56 -0300

    Configuração heroku

--- taf/settings.py
+++ taf/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
+import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,10 +118,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-import os
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+django_heroku.settings(locals())
